eb423fc30d5135bb15056345a5f9f4cad1c7a258
yurchikKon <157982309+yurchikKon@users.noreply.github.com>
2024-05-20 00:05:08 +0300

    Update SimpleServlet.java

--- src/main/java/org/example/SimpleServlet.java
+++ src/main/java/org/example/SimpleServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/hello")
 public class SimpleServlet extends HttpServlet {
 
     @Override
