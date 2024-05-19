5975d8644c12fb7601fc8c237f8e6682c8cae93e
Юра Кондратенко <antooon050902@yandex.com>
2024-05-19 14:27:25 +0300

    pom changes

--- src/main/java/org/example/SimpleServlet.java
+++ src/main/java/org/example/SimpleServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/")
 public class SimpleServlet extends HttpServlet {
 
     @Override
