c73b171fecc052777ec793b6bc2b83606fd2775f
Юра Кондратенко <antooon050902@yandex.com>
2024-05-23 13:51:30 +0300

    endpoint changed

--- src/main/java/org/example/SimpleServlet.java
+++ src/main/java/org/example/SimpleServlet.java
@@ -8,14 +8,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/")
+@WebServlet("/hello")
 public class SimpleServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
-        writer.write("Hello Yura!!!!!");
+        writer.write("Hello World!!!!!");
         writer.close();
     }
 }
